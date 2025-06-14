166b240f9a6db1617c9460aa30ac4118f4cfbaf3
Stig Grindland <stig.grindland@gmail.com>
2025-06-14 11:28:53 +0200

    🚨 HOTFIX: Fix import names in tasks/__init__.py - fetch_nasa_fires_data → fetch_nasa_fires, fetch_openaq_data → fetch_openaq_latest

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -3,6 +3,6 @@ Task modules for environmental data collection
 """
 from .fetch_openweathermap_weather import fetch_weather_data
 from .fetch_gbif_biodiversity import fetch_biodiversity_data
-from .fetch_nasa_fires import fetch_nasa_fires_data
-from .fetch_openaq_latest import fetch_openaq_data
+from .fetch_nasa_fires import fetch_nasa_fires
+from .fetch_openaq_latest import fetch_openaq_latest
 from .fetch_noaa_ocean import fetch_noaa_ocean_data
